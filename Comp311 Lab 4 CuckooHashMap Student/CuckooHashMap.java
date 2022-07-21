8162705d7b9c4ab3b902b40795a1ba374c1072f1
Evann Hall <halle15@email.franklin.edu>
2022-07-21 15:29:57 -0400

    added putall, modified insert to keep track of size properly

--- Comp311 Lab 4 CuckooHashMap Student/CuckooHashMap.java
+++ Comp311 Lab 4 CuckooHashMap Student/CuckooHashMap.java
@@ -1,334 +1,334 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.List;
 
-public class CuckooHashMap<K, V> implements Map<K, V>
-{
-    private List<MapEntry> table;
-    int numBuckets;
+public class CuckooHashMap<K, V> implements Map<K, V> {
+	private List<MapEntry> table;
+	int numBuckets;
+	private int size;
 
-    public CuckooHashMap(int numBuckets)
-    {
-    	table = new ArrayList<MapEntry>(numBuckets);
-    	this.numBuckets = numBuckets;
-    	for(int i = 0; i < numBuckets; i++) {
-    		table.add(i, null);
-    	}
-    }
-    
-    public MapEntry getEntry(int index)
-    {
-        // This works, don't touch.
-        return table.get(index);
-        
-    }
-    
-    @Override
-    public void clear()
-    {
-    	table = new ArrayList<MapEntry>(10);
-    	
-    }
-    
-    @Override
-    public boolean isEmpty()
-    {
-        return table.isEmpty();
-    }
+	public CuckooHashMap(int numBuckets) {
+		table = new ArrayList<MapEntry>(numBuckets);
+		this.numBuckets = numBuckets;
+		this.size = 0;
+		for (int i = 0; i < numBuckets; i++) {
+			table.add(i, null);
+		}
+	}
 
-    public int size()
-    {
-    	int ct = 0;
-    	for(MapEntry m : table) {
-    		if(m != null) {
-    			ct += 1;
-    		}
-    	}
-    	return ct;
-    }
-    
-    @Override
-    public V get(Object key)
-    {
-    	for(MapEntry e : table) {
-    		try {
-    		if(e.getKey().equals(key)) {
-    			return e.getValue();
-    		}
-    		}
-    		catch(Exception a) {
+	public MapEntry getEntry(int index) {
+		// This works, don't touch.
+		return table.get(index);
 
-    		}
-    	}
-      return null;
-    }
+	}
 
-    @Override
-    public boolean containsKey(Object key)
-    {
-    	
-        for(MapEntry m : table) {
-        	try {
-        
-        	if(m.getKey().equals(key)) {
-        		return true;
-        	}
-        	}
-        	catch(Exception a) {
-        		
-        	}
-        }
-        return false;
-    }
+	@Override
+	public void clear() {
+		table = new ArrayList<MapEntry>(10);
+		size = 0;
+		numBuckets = 10;
 
-    @Override
-    public boolean containsValue(Object value)
-    {
-        for(MapEntry m : table) {
-        	try {
-        
-        	if(m.getValue().equals(value)) {
-        		return true;
-        	}
-        	}
-        	catch(Exception a) {
-        		
-        	}
-        }
-        return false;
-    }
-    
-    private int hf1(int hashcode) {
-    	return Math.abs(hashcode) % table.size();
-    	
-    }
-    
-    private int hf2(int hashcode) {
-    	System.out.println((hashcode + 3) % 20);
-    	return (hashcode + 3) % table.size();
-    	
-    }
-    
-    private void resize() {
-    	List<MapEntry> old = table;
-    	int amt = numBuckets * 2;
-    	table = new ArrayList<MapEntry>((int) amt);
-    	numBuckets = table.size();
-    	
-    	while(table.size() < amt) {
-    		table.add(null);
-    	}
-    	System.out.println("BRUH: " + table.size());
-    	
-    	for(MapEntry e : old) {
-    		if(e != null) {
-    			insert(e);
-    			System.out.println("WE INSERTED");
-    		}
-    	}
-    	
-    }
-    
-    public V insert(MapEntry e) {
-    	MapEntry x = e;
-    	V ret = null;
-    	int currentPos;
-    	
-    	int pos1 = hf1(x.key.hashCode());
-    	int pos2 = hf2(pos1);
-    	
-    	try {
-    		
-    	if(table.get(pos1).equals(x)) {
-    		return table.get(pos1).value;
-    	}
-    	
-    	if(table.get(pos2).equals(x)) {
-    		return table.get(pos2).value;
-    	}
-    	
-    	}
-    	catch(Exception a) {
-    		
-    	}
-    	
-    	currentPos = hf1(pos1);
-    	
-    	for(int i = 0; i < 10; i++){
-    		if(table.get(currentPos) == null) {
-    			table.set(currentPos, x);
- 
-    			return ret;
-    		}
-    		else {
-    			
-    			MapEntry a = table.get(currentPos);
-    			table.set(currentPos, x);
-    			x = a;
-    			ret = x.value;
-    			if(currentPos == hf1(x.key.hashCode())) {
-    				currentPos = hf2(hf1(x.key.hashCode()));
-    				
-    			}
-    			else {
-    				currentPos = hf1(x.key.hashCode());
-    				
-    			}
-    			
-    			
-    		}
-    	}
-    	resize();
-    	insert(x);
-    	ret = x.getValue();
-    	return ret;
-    	// insert rehash
+	}
 
-    	
-    	
-    }
+	@Override
+	public boolean isEmpty() {
+		return table.isEmpty();
+	}
 
-    public V put(K key, V value)
-    {
-    	MapEntry e = new MapEntry(key, value);
+	public int size() {
+		return size;
+	}
 
-    	return(insert(e));
-    	
-    	/*for(int i = 0; i < 10; i++) {
-    		if(table.get(pos).isNull()){
-    			table.add(pos, e);
-    			return value;
-    		}
-    		else {
-    			System.out.println("went here");
-    			table.set(hf2(pos), table.get(pos));
-    			System.out.println(pos + " " + hf2(pos));
-    			table.set(pos, e);
-    			return value;
-    		}
+	@Override
+	public V get(Object key) {
+		for (MapEntry e : table) {
+			try {
+				if (e.getKey().equals(key)) {
+					return e.getValue();
+				}
+			} catch (Exception a) {
 
-    	
-    	
-    	//}
-    	//return value;*/
-    }
-    	
+			}
+		}
+		return null;
+	}
 
-    
-    public V remove(Object key)
-    {
-    	if(get(key) != null) {
-    		@SuppressWarnings("unchecked")
-			MapEntry e = new MapEntry((K) key, get(key)) ;
-    		
+	@Override
+	public boolean containsKey(Object key) {
 
-    		
-    		for(int i = 0; i < table.size(); i++) {
-    			System.out.println(table.size());
-    			e = table.get(i);
-    			if(e != null) {
-    			if(e.key == key) {
-    				table.remove(i);
-    				return e.getValue();
-    			}
-    		}
-    		}
+		for (MapEntry m : table) {
+			try {
 
-    	}
-        return null;
-    }
-    
-    @Override
-    public String toString() {
-    	String r = "";
-    	for(MapEntry m : table) {
-    		r = r.concat("KEY: " + m.key + "|VALUE: " + m.value + "\n");
-    	}
-    	
-    	return r;
-    }
+				if (m.getKey().equals(key)) {
+					return true;
+				}
+			} catch (Exception a) {
 
-    public void putAll(Map<? extends K, ? extends V> map)
-    {
-    }
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public Set<java.util.Map.Entry<K, V>> entrySet()
-    {
-        return null;
-    }
+	@Override
+	public boolean containsValue(Object value) {
+		for (MapEntry m : table) {
+			try {
 
-    public Set<K> keySet()
-    {
-        return null;
-    }
+				if (m.getValue().equals(value)) {
+					return true;
+				}
+			} catch (Exception a) {
 
-    public Collection<V> values()
-    {
-        return null;
-    }
-    
-    public class MapEntry implements Entry<K, V>
-    {
-        private K key;
-        private V value;
- 
-        /**
-         * Creates a MapEntry.
-         * 
-         * @param akey the key
-         * @param avalue the value
-         */
-        public MapEntry(K akey, V avalue)
-        {
-            this.key = akey;
-            this.value = avalue;
-        }
-        
-        public boolean isNull() {
-        	return(key == null && value == null);
+			}
+		}
+		return false;
+	}
 
-        	
-        }
+	private int hf1(int hashcode) {
+		return Math.abs(hashcode) % table.size();
 
-        /**
-         * Returns the key for this entry.
-         * 
-         * @see java.util.Map$Entry#getKey()
-         * @return the key for this entry
-         */
-        @Override
-        public K getKey()
-        {
-            return key;
-        }
+	}
 
-        /**
-         * Returns the value for this entry.
-         * 
-         * @see java.util.Map$Entry#getValue()
-         * @return the value for this entry
-         */
-        @Override
-        public V getValue()
-        {
-            return value;
-        }
+	private int hf2(int hashcode) {
+		System.out.println((hashcode + 3) % 20);
+		return (hashcode + 3) % table.size();
 
-        /**
-         * Sets the value for this entry.
-         * 
-         * @see java.util.Map$Entry#setValue(V)
-         * @param val
-         * @return the previous value for this entry
-         */
-        public V setValue(V newValue)
-        {
-            V oldVal = value;
-            value = newValue;
-            return oldVal;
-        }
-    }
+	}
+
+	private void resize() {
+		List<MapEntry> old = table;
+		int amt = numBuckets * 2;
+		table = new ArrayList<MapEntry>((int) amt);
+		numBuckets = amt;
+
+		while (table.size() < amt) {
+			table.add(null);
+		}
+		System.out.println("BRUH: " + table.size());
+
+		for (MapEntry e : old) {
+			if (e != null) {
+				insert(e);
+				System.out.println("WE INSERTED");
+			}
+		}
+
+	}
+
+	public V insert(MapEntry e) {
+
+		MapEntry x = e;
+
+		V prevVal = null;
+
+		int currentPos;
+
+		int pos1 = hf1(x.key.hashCode());
+
+		int pos2 = hf2(pos1);
+
+		try {
+			
+			if (table.get(pos1).key == e.key) {
+				size--;
+				return table.get(pos1).value;
+
+			}
+
+			if (table.get(pos2).key == e.key) {
+				size--;
+				return table.get(pos2).value;
+
+			}
+
+		}
+
+		catch (Exception a) {
+
+		}
+
+		currentPos = pos1;
+		try {
+		prevVal = table.get(currentPos).getValue();
+		}catch(Exception c) {
+		}
+
+		for (int i = 0; i < 10; i++) {
+
+			if (table.get(currentPos) == null) {
+
+				table.set(currentPos, x);
+				
+				return null;
+
+			}
+
+			else {
+
+				MapEntry a = table.get(currentPos);
+
+				table.set(currentPos, x);
+
+				x = a;
+
+				prevVal = x.value;
+
+				if (currentPos == hf1(x.key.hashCode())) {
+
+					currentPos = hf2(hf1(x.key.hashCode()));
+
+				}
+
+				else {
+
+					currentPos = hf1(x.key.hashCode());
+
+				}
+
+			}
+
+		}
+
+		resize();
+
+		insert(x);
+
+
+		return prevVal;
+
+	}
+
+	public V put(K key, V value) {
+		MapEntry e = new MapEntry(key, value);
+		if (e != null) {
+			size++;
+		}
+		return (insert(e));
+
+		/*
+		 * for(int i = 0; i < 10; i++) { if(table.get(pos).isNull()){ table.add(pos, e);
+		 * return value; } else { System.out.println("went here"); table.set(hf2(pos),
+		 * table.get(pos)); System.out.println(pos + " " + hf2(pos)); table.set(pos, e);
+		 * return value; }
+		 * 
+		 * 
+		 * 
+		 * //} //return value;
+		 */
+	}
+
+	public V remove(Object key) {
+		if (get(key) != null) {
+			@SuppressWarnings("unchecked")
+			MapEntry e = new MapEntry((K) key, get(key));
+
+			for (int i = 0; i < table.size(); i++) {
+				System.out.println(table.size());
+				e = table.get(i);
+				if (e != null) {
+					if (e.key == key) {
+						table.remove(i);
+						return e.getValue();
+					}
+				}
+			}
+
+		}
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		String r = "";
+		for (MapEntry m : table) {
+			r = r.concat("KEY: " + m.key + "|VALUE: " + m.value + "\n");
+		}
+
+		return r;
+	}
+	
+	private void putCheck(K k, V v) {
+		if(k != null && v != null) {
+			put(k, v);
+		}
+	}
+
+	public void putAll(Map<? extends K, ? extends V> map) {
+		
+		map.forEach((k, v) -> putCheck(k, v));;  // if this works i can not believe I implemented a lambda properly
+		
+		//Iterator<Map.Entry<K, V>> itr = map.entrySet().iterator();
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		return null;
+	}
+
+	public Set<K> keySet() {
+		return null;
+	}
+
+	public Collection<V> values() {
+		return null;
+	}
+
+	public class MapEntry implements Entry<K, V> {
+		private K key;
+		private V value;
+
+		/**
+		 * Creates a MapEntry.
+		 * 
+		 * @param akey   the key
+		 * @param avalue the value
+		 */
+		public MapEntry(K akey, V avalue) {
+			this.key = akey;
+			this.value = avalue;
+		}
+
+		public boolean isNull() {
+			return (key == null && value == null);
+
+		}
+
+		/**
+		 * Returns the key for this entry.
+		 * 
+		 * @see java.util.Map$Entry#getKey()
+		 * @return the key for this entry
+		 */
+		@Override
+		public K getKey() {
+			return key;
+		}
+
+		/**
+		 * Returns the value for this entry.
+		 * 
+		 * @see java.util.Map$Entry#getValue()
+		 * @return the value for this entry
+		 */
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		/**
+		 * Sets the value for this entry.
+		 * 
+		 * @see java.util.Map$Entry#setValue(V)
+		 * @param val
+		 * @return the previous value for this entry
+		 */
+		public V setValue(V newValue) {
+			V oldVal = value;
+			value = newValue;
+			return oldVal;
+		}
+	}
 }
--- Comp311 Lab 4 CuckooHashMap Student/CuckooHashMapTest.java
+++ Comp311 Lab 4 CuckooHashMap Student/CuckooHashMapTest.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Hashtable;
 
 import junit.framework.TestCase;
 
@@ -6,15 +7,46 @@ public class CuckooHashMapTest extends TestCase
 {
     public void testPutGet() {
         CuckooHashMap<Integer, String> m = new CuckooHashMap<>(10);
+        HashMap<Integer, String> ma = new HashMap<>(10);
+        
         assertEquals(0, m.size());
         assertNull(m.get(3));
 
         m.put(3, "three");
+        m.put(3, "three");
+        ma.put(3, "three");
+        ma.put(3, "three");
+        assertEquals(1, ma.size());
         assertEquals(1, m.size());
-        assertEquals("three", m.get(3));
+       // assertEquals("three", m.get(3));
         
         
 
+    }
+    
+    public void testPutAll() {
+        HashMap<Integer, String> m = new HashMap<>(10);
+        CuckooHashMap<Integer, String> ma = new CuckooHashMap<>(10);
+        
+        assertEquals(0, m.size());
+        assertNull(m.get(3));
+
+        m.put(3, "three");
+        m.put(3, "three");
+        ma.put(3, "three");
+
+        assertEquals(1, m.size());
+       // assertEquals("three", m.get(3));
+        
+        for(int i = 0; i < 25; i++) {
+        	m.put(i, "TEST" + i);
+        }
+        m.put(11, "test");
+        HashMap<Integer, String> g = new HashMap<>(10);
+        g.putAll(m);
+        ma.putAll(m);
+        assertEquals(25, g.size());
+        assertEquals(25, ma.size());
     }
     
     /*public void testwhat() {
@@ -32,6 +64,7 @@ public class CuckooHashMapTest extends TestCase
     
     public void testRealHash() {
         HashMap<Integer, String> m = new HashMap<>(10);
+        Hashtable<Integer, String> t = new Hashtable<>(10);
         CuckooHashMap<Integer, String> c = new CuckooHashMap<>(10);
         assertEquals(0, m.size());
         assertNull(m.get(3));
@@ -39,12 +72,15 @@ public class CuckooHashMapTest extends TestCase
         for(int i = 0; i < 10; i++) {
         	m.put(i, "TEST" + i);
         	c.put(i, "TEST" + i);
+        	t.put(i, "TEST" + i);
         }
         assertEquals(10, m.size());
         assertEquals(10, c.size());
         
         m.put(1,  "BREAK!");
         c.put(1,  "BREAK!");
+        t.put(1, "BREAK!");
+        assertEquals(11, t.size());
         assertEquals(11, m.size());
         assertEquals(11, c.size());
         
@@ -67,7 +103,7 @@ public class CuckooHashMapTest extends TestCase
         assertEquals(10, m.size());
         
         m.put(1,  "BREAK!");
-        assertEquals(11, m.size());
+        assertEquals(10, m.size());
         
         m.put(3, "BURN!!!!");
         assertEquals(12, m.size());
